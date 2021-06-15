fa97aaea902e4d317c99417c85529a962bcd4b3c
koenkeune <koenkeune@live.nl>
2021-06-15 14:50:03 +0200

    prints the standing info as well now

--- visualizer.py
+++ visualizer.py
@@ -1,5 +1,24 @@
 from static import *
+from calculations import getExpectedWinPct
 
-def printRanking(ranking):
-    for i in range(len(ranking)):
-        print(i+1, TEAMS_INFO[ranking[i][0]][5])
+def printRanking(r, standings):
+    rSorted = sorted(r.items(), key=lambda x: x[1], reverse=True)
+    for i in range(len(rSorted)):
+        abr = rSorted[i][0]
+        win_pct = rSorted[i][1]
+        team = TEAMS_INFO[abr][5]
+        streak = 'none'
+        if standings[abr][5][0] == 'W':
+            streak = 'wins'
+            if standings[abr][5][1] == 1:
+                streak = 'win' 
+        elif standings[abr][5][0] == 'L':
+            streak = 'losses'
+            if standings[abr][5][1] == 1:
+                streak = 'loss'
+        print(i+1, team, '  Record:', standings[abr][1], '-', standings[abr][2], 
+              ', exp win pct:', round(getExpectedWinPct(rSorted[i][1], 1500) * 100), ', win pct:', 
+              round(standings[abr][3] * 100), ', last 10:', standings[abr][4], ', streak:', 
+              standings[abr][5][1], streak)
+        
+            
