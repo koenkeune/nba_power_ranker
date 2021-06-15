72cdd61ad9aaaaa607f0467ad9bc81d29734d537
koenkeune <koenkeune@live.nl>
2021-06-15 14:49:45 +0200

    runs from command line now

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+import sys
 from ranking import *
 from visualizer import *
 
-date = '2021-02-15'
-r = rankTeams(2020, date)
+if len(sys.argv) == 1:
+    date = '2021-02-15'
+elif len(sys.argv) == 2:
+    date = int(sys.argv[1])
+else:
+    sys.exit('first (optional) argument should be the date in yyyy-mm-dd format')
 
-rSorted = sorted(r.items(), key=lambda x: x[1], reverse=True)
+season = 2020
+r = rankTeams(season, date)
 
+standings = getStandings(season, date)
+printRanking(r, standings)
 
-
-
-
-printRanking(rSorted)
-
-#standings = getStandings(2020, END_OF_SEASON_2020)
